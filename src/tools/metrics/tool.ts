12a5aa28b4c14906b1d231ef822171a5738dd3b1
zigelboim-misha <zigelboim.misha@gmail.com>
2025-07-24 16:19:15 +0300

    feat: add search_metrics tool to metrics handler and update imports

--- src/tools/metrics/tool.ts
+++ src/tools/metrics/tool.ts
@@ -1,9 +1,9 @@
 import { ExtendedTool, ToolHandlers } from '../../utils/types'
 import { v1 } from '@datadog/datadog-api-client'
 import { createToolSchema } from '../../utils/tool'
-import { QueryMetricsZodSchema } from './schema'
+import { QueryMetricsZodSchema, GetMetricsZodSchema } from './schema'
 
-type MetricsToolName = 'query_metrics'
+type MetricsToolName = 'query_metrics' | 'search_metrics'
 type MetricsTool = ExtendedTool<MetricsToolName>
 
 export const METRICS_TOOLS: MetricsTool[] = [
@@ -11,6 +11,11 @@ export const METRICS_TOOLS: MetricsTool[] = [
     QueryMetricsZodSchema,
     'query_metrics',
     'Query timeseries points of metrics from Datadog',
+  ),
+  createToolSchema(
+    GetMetricsZodSchema,
+    'search_metrics',
+    'Searches a sub-string from all DataDog metrics names. Please provide a query string to filter the metrics.',
   ),
 ] as const
 
@@ -24,18 +29,24 @@ export const createMetricsToolHandlers = (
       const { from, to, query } = QueryMetricsZodSchema.parse(
         request.params.arguments,
       )
-
-      const response = await apiInstance.queryMetrics({
-        from,
-        to,
-        query,
-      })
-
+      const response = await apiInstance.queryMetrics({ from, to, query })
       return {
         content: [
           {
             type: 'text',
             text: `Queried metrics data: ${JSON.stringify({ response })}`,
+          },
+        ],
+      }
+    },
+    search_metrics: async (request) => {
+      const { q } = GetMetricsZodSchema.parse(request.params.arguments)
+      const response = await apiInstance.listMetrics({ q: q || '*' })
+      return {
+        content: [
+          {
+            type: 'text',
+            text: `Fetched metrics: ${JSON.stringify(response)}`,
           },
         ],
       }
